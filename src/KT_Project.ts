4ee81049096090e3d80e3ac937658609ed54d8e1
octopodo <orson_wells40@hotmail.com>
2025-11-05 10:07:49 +0100

    Restore KT_ProjectDuplicate import and method assignment for completeness

--- src/KT_Project.ts
+++ src/KT_Project.ts
@@ -3,7 +3,8 @@ import { KT_ProjectImport } from "./import";
 import { KT_ProjectAdd } from "./add";
 import { KT_ProjectMove } from "./move";
 import { KT_ProjectRemove } from "./remove";
-// import { KT_ProjectDuplicate } from "./duplicate";
+import { KT_ProjectDuplicate } from "./duplicate";
+import { KT_AeProjectPath } from "./path";
 class __KT_Project {
     private name = "KtAeProject";
     private version = "1.0.0";
@@ -12,8 +13,8 @@ class __KT_Project {
     public add = KT_ProjectAdd;
     public move = KT_ProjectMove;
     public remove = KT_ProjectRemove;
-    // public duplicate = KT_ProjectDuplicate;
-
+    public duplicate = KT_ProjectDuplicate;
+    public path = KT_AeProjectPath;
     constructor() {
         $.writeln("KtAeProject constructor");
     }
