8479bb9b9864fa20e58c1b4bf408920951cb2980
octopodo <orson_wells40@hotmail.com>
2025-12-10 12:34:50 +0100

    Remove unused __KT_OpenDialog class and its methods to clean up the codebase

--- src/openDialog.ts
+++
@@ -1,9 +0,0 @@
-class __KT_OpenDialog {
-    // file = () => { {
-    //     return
-    // }
-
-    folder = (prompt?: string) => {
-        return File.saveDialog(prompt || "Select a a file");
-    };
-}
